d1bebdd1062617d2bbb6e82eacbec8c318c087f0
Divine Chidozie <igwedivine92@gmail.com>
2025-12-24 05:21:57 +0100

    inital commit

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-// Remember to uncomment the code below later
-// document.getElementById("year").textContent = new Date().getFullYear();
-
 const getInTouch = document.getElementById("getInTouch");
+const downloadButton = document.getElementById("download-btn");
+const navHeader = document.getElementById("nav-header");
+const navLogo = document.querySelector(".logo");
+const navListItems = document.querySelectorAll(".nav-list-items");
+const menu = document.getElementById("menu");
+const mobileMenu = document.getElementById("mobileMenu");
+
+const navHeaderBackground = `#9318fa`;
+const backgroundTransition = "0.3s ease-in";
+const newNavLogo = "white";
+
+function navHeaderScroll() {
+  if (window.scrollY > 50) {
+    navHeader.style.backgroundColor = `${navHeaderBackground}`;
+    navHeader.style.transition = `${backgroundTransition}`;
+    navLogo.style.color = newNavLogo;
+  } else {
+    navHeader.style.backgroundColor = "";
+    navLogo.style.color = "#0000EE";
+  }
+}
+window.addEventListener("scroll", navHeaderScroll);
+
+menu.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active");
+});
 
 getInTouch.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("Get in Touch Button clicked..");
 });
+
+downloadButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert("Download CV button clicked");
+});
+
+const year = document.getElementById("year");
+year.textContent = new Date().getFullYear();
